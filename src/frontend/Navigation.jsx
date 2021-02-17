97543bb9f0da681fd35ce7f5c02548f4ed3553c3
liannedls <liannedelasalle@gmail.com>
2021-02-16 23:59:42 -0600

    new navbar

--- src/frontend/Navigation.jsx
+++ src/frontend/Navigation.jsx
@@ -1,28 +1,31 @@
-import React, { Component } from 'react'
-
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Form, FormControl, Button, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 class Navigation extends Component {
 
   render() {
     return (
         <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-        <div className="container">
-        <div className="navbar-header">
-        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span className="sr-only">Toggle navigation</span> <span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span>
-        </button>
-
-          <Link to="/" className="navbar-brand page-scroll" >Famille de la Salle</Link>
-        </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-center">
-              <li><Link to="/tree" >Family Tree</Link></li>
-              <li><Link to="/contact" >Contact</Link></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#tree">Tree</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        </nav>
     )
   }
 }
