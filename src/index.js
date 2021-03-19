5d61132e6cbb4d96657f52a997f6f04b5b13ffec
liannedls <liannedelasalle@gmail.com>
2021-03-19 13:44:01 -0400

    build

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './frontend/App';
-import Timelinepage from './frontend/TimelinePage';
-import Tree from './frontend/Tree';
-import Contact from './frontend/Contact';
-import { HashRouter, Route } from 'react-router-dom'
-import Navigation from './frontend/Navigation';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./frontend/App";
+import Timelinepage from "./frontend/TimelinePage";
+import Tree from "./frontend/Tree";
+import Contact from "./frontend/Contact";
+import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./frontend/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const routing = (
-
-   <HashRouter basename='/'>
+  <HashRouter basename="/">
     <div>
       <Navigation />
-    <Route exact path="/" component={App} />
+      <Route exact path="/" component={App} />
       <Route path="/timelinepage" component={Timelinepage} />
       <Route path="/contact" component={Contact} />
       <Route path="/tree" component={Tree} />
     </div>
+  </HashRouter>
+);
 
-    </HashRouter>
-)
-
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(routing, document.getElementById("root"));
