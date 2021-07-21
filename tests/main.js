7f98bace38ac0614e7e011c252d59e87dfe7877b
Lianne <liannedelasalle@gmail.com>
2021-07-21 15:49:17 -0400

    Delete tests directory

--- tests/main.js
+++
@@ -1,8 +0,0 @@
-import assert from "assert";
-
-describe("landingpage-react-template", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "landingpage-react-template");
-  });
-});
